c9767e2ba1e40d5bf01a77ad74a6936c5896c067
Gal Gordon <gal.gordon@gmail.com>
2019-04-27 19:12:58 +0300

    initial commit

---
+++ BeeOnTime/BeeOnTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeeOnTime
+//
+//  Created by Gal Gordon on 27/04/2019.
+//  Copyright © 2019 Gal Gordon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
